40c84e10456d434fa3fc5f9e464317b11f2162f9
Terry Wilson <twilson@redhat.com>
2020-07-06 16:48:49 -0500

    Update for my purposes
    Dirty all-in-one commit of:
    
    * Fix for Python 3
    * Formatting changes
    * Add support for moving cards that haven't been updated in n days
      to their own "Not updated" label. Also don't display comments
      older than this in the report.
    * Make board name, in progress/done lists names, and top/bottom
      labels configurable
    * Fix some trailing whitespace/pep8 issues

--- trello_report/reporter.py
+++ trello_report/reporter.py
@@ -13,6 +13,7 @@ except ImportError:
     import ConfigParser as configparser
 
 import datetime
+import dateutil.parser
 import os
 import sys
 import textwrap
@@ -24,30 +25,40 @@ from trello import trelloclient
 SKIP_LABEL = 'Skip-for-report'
 
 
-COMMENT_INDENT = '  '
-URL_INDENT = '  -> '
+BASE_INDENT = '  '
+COMMENT_INDENT_INIT = BASE_INDENT + '- '
+COMMENT_INDENT = BASE_INDENT + '  '
+URL_INDENT = BASE_INDENT + '-> '
 
+base_wrapper = textwrap.TextWrapper(
+    initial_indent=BASE_INDENT, subsequent_indent=BASE_INDENT)
 comment_wrapper = textwrap.TextWrapper(
-    initial_indent=COMMENT_INDENT, subsequent_indent=COMMENT_INDENT)
+    initial_indent=COMMENT_INDENT_INIT, subsequent_indent=COMMENT_INDENT)
 
 
 class CardData(object):
-    def __init__(self, card):
+    def __init__(self, card, updates_since=None):
         super(CardData, self).__init__()
         self._card = card
         # cache data to avoid later refetches
-        self._comments = card.get_comments()
+        self._comments = [
+            c for c in card.get_comments()
+            if (updates_since is None
+                or dateutil.parser.isoparse(c['date']) > updates_since)]
         self._attachments = card.get_attachments()
         self._checklists = card.fetch_checklists()
 
+    def __hash__(self):
+        return self._card.__hash__()
+
     @property
     def name(self):
-        return self._card.name.encode('utf-8')
+        return self._card.name
 
     @property
     def description(self):
         desc = self._card.desc
-        return desc.encode('utf-8') if desc else None
+        return desc if desc else None
 
     @property
     def labels(self):
@@ -61,7 +72,7 @@ class CardData(object):
         for comment in self._comments:
             if comment['type'] == 'commentCard':
                 if 'data' in comment and 'text' in comment['data']:
-                    l.append(comment['data']['text'].encode('utf-8'))
+                    l.append(comment['data']['text'])
         return l
 
     @property
@@ -73,34 +84,40 @@ class CardData(object):
 
     @property
     def checklists(self):
-        l = []  
+        l = []
         for checklist in self._checklists:
             d = {'name': checklist.name, 'items': []}
             for n in checklist.items:
-                d['items'].append("%s %s" % ('[X]' if n['checked'] else '[ ]', n['name']))
+                d['items'].append(
+                    "%s %s" % ('[X]' if n['checked'] else '[ ]', n['name']))
             l.append(d)
         return l
 
+    @property
+    def date_last_activity(self):
+        return self._card.date_last_activity
+
+    def updated_since(self, timestamp):
+        return timestamp is None or self.date_last_activity > timestamp
+
     def __str__(self):
-        res = '\n[*] '
+        res = '[*] '
         res += self.name
         if self.description:
-            res = '%(res)s\n%(desc)s' % {'res': res, 'desc': self.description}
+            res += "\n" + "\n".join(base_wrapper.wrap(self.description))
         comments = list(self.comments)
 
         if comments:
             for comment in comments:
                 res += '\n'
-                res += comment
-                # res += '\n'.join(comment_wrapper.wrap(comment))
-        
+                res += '\n'.join(comment_wrapper.wrap(comment))
+
         for checklist in self.checklists:
-            res += '\n'
-            res += checklist['name']    
-            res += '\n'
+            res += '\n' + BASE_INDENT
+            res += checklist['name'] or "Checklist"
             for element in checklist['items']:
                 res += '\n'
-                res += COMMENT_INDENT
+                res += BASE_INDENT * 2
                 res += element
 
         attachments = self.attachments
@@ -109,10 +126,10 @@ class CardData(object):
             counter = 0
             for attachment in attachments:
                 res += '\n'
-                # res += URL_INDENT
+                res += URL_INDENT
                 res += '[%d] ' % counter
                 counter += 1
-                res += attachment.decode('utf-8')        
+                res += attachment
         res += '\n'
         return res
 
@@ -123,7 +140,7 @@ def get_board(api, name):
     return board[0] if board else None
 
 
-def get_board_labels(board):
+def get_board_labels(board, top_labels, bottom_labels):
     labels = {l.name for l in board.get_labels()
               if l.name != SKIP_LABEL}
 
@@ -131,14 +148,14 @@ def get_board_labels(board):
     res = []
 
     # first, put high visible topics at the top
-    for label in ('Customer cases / Escalations', 'Management', 'OVS/OVN'):
+    for label in top_labels:
         if label in labels:
             res.append(label)
             labels.remove(label)
 
     # include uncategorized topics except those that go at the very end
     for label in set(labels):
-        if label not in ('Organizational',):
+        if label not in bottom_labels:
             res.append(label)
             labels.remove(label)
 
@@ -153,10 +170,18 @@ def get_list(board, name):
     return list_[0] if list_ else None
 
 
-def get_cards(l):
-    cards = {CardData(card) for card in l.list_cards()}
-    return {card for card in cards
-            if SKIP_LABEL not in card.labels}
+def get_cards(l, updated_since=None):
+    updated_cards = set()
+    stale_cards = set()
+    for card in l.list_cards():
+        card = CardData(card, updates_since=updated_since)
+        if SKIP_LABEL in card.labels:
+            continue
+        if card.updated_since(updated_since):
+            updated_cards.add(card)
+        else:
+            stale_cards.add(card)
+    return updated_cards, stale_cards
 
 
 def get_cards_by_label(cards, label):
@@ -164,17 +189,17 @@ def get_cards_by_label(cards, label):
 
 
 def _print_label_header(label):
-    print label
-    print '~' * len(label)
-    print
+    print(label)
+    print('~' * len(label))
+    print()
 
 
 def _print_list_header(l):
     name = l.name
-    print name
-    print '=' * len(name)
-    print '=' * len(name)
-    print
+    print('=' * len(name))
+    print(name)
+    print('=' * len(name))
+    print()
 
 
 # copy-pasted from https://github.com/rbrady/filch/blob/master/filch/helpers.py
@@ -200,44 +225,57 @@ def get_config_info():
     return trello_data
 
 
-def main():
+@click.command()
+@click.option('--days', type=int, default=None,
+              help="Cards not updated in n days go in their own section")
+def main(days):
     config_info = get_config_info()
     api = trelloclient.TrelloClient(api_key=config_info['api_key'],
                                     token=config_info['access_token'])
 
-    b = get_board(api, 'Daniel\'s tasks')
+    b = get_board(api, config_info['board'])
     assert b is not None
 
-    in_progress_list = get_list(b, 'In progress')
+    in_progress_list = get_list(b, config_info['in_progress_list'])
     assert in_progress_list is not None
 
-    done_list = get_list(b, 'Done')
+    done_list = get_list(b, config_info['done_list'])
     assert done_list is not None
 
-    print 'Report generated on: %s.' % datetime.date.today().isoformat()
-    print 'Generated by: https://github.com/booxter/trello-report\n'
+    top_labels = [l.strip() for l in config_info['top_labels'].split(',')]
+    end_labels = [l.strip() for l in config_info['bottom_labels'].split(',')]
 
-    labels = get_board_labels(b)
+    print('Report generated on: %s.' % datetime.date.today().isoformat())
+    print('Generated by: https://github.com/booxter/trello-report\n')
 
+    labels = get_board_labels(b, top_labels, end_labels)
+    since = (datetime.datetime.now(datetime.timezone.utc)
+             - datetime.timedelta(days=days)) if days else None
     for l in (in_progress_list, done_list):
         _print_list_header(l)
-        cards = get_cards(l)
+        cards, not_updated = get_cards(l, updated_since=since)
         for label in labels:
             labeled_cards = get_cards_by_label(cards, label)
             if labeled_cards:
                 _print_label_header(label)
                 for card in labeled_cards:
-                    print card
+                    print(card)
                     cards.remove(card)
 
         # handle remaining, unlabeled cards
         if cards:
             _print_label_header('Other')
             for card in cards:
-                print card
+                print(card)
+
+        # special section for cards that haven't had updates this week
+        if not_updated:
+            _print_label_header('Not updated')
+            for card in not_updated:
+                print(card)
 
     # finally, archive all cards that we just reported on
-    #done_list.archive_all_cards()
+    # done_list.archive_all_cards()
 
 
 if __name__ == '__main__':
